2c48e7019dbc144b72b41c809f122c5e2dec4914
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-19 18:42:35 +0530

    ✅ Pair With Difference | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ gfg/easy/find-pair-given-difference1559/solution.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public boolean findPair(int[] arr, int x) 
+    {
+        // code here
+        
+       HashSet<Integer> set=new HashSet<>();
+       
+       for(int num :arr)
+       {
+           if(set.contains(num-x)|| set.contains(num+x))
+           {
+               return true;
+           }
+           
+           set.add(num);
+       }
+       
+       return false;
+    }
+}
