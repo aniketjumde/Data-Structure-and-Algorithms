ca322d69437e59f1069cc74f32e03db6ca5c061d
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-20 11:55:13 +0530

    ✅ Fibonacci Number | Easy | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-08-20

--- leetcode/easy/0509-fibonacci-number/solution.java
+++ leetcode/easy/0509-fibonacci-number/solution.java
@@ -6,16 +6,8 @@ class Solution {
             return n;
         }
 
-        int f0=0;
-        int f1=1;
-        int f3=0;
-        for(int i=2;i<=n;i++)
-        {
-            f3=f0+f1;
-            f0=f1;
-            f1=f3;
-        }  
+        
 
-        return  f3;
+        return fib(n-1)+fib(n-2);
     }
 }
