5f500c1134c7c2efb76d6dfde59bb56ebca27402
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-23 18:26:23 +0530

    ✅ 1 to n Without Loops | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ gfg/basic/print-1-to-n-without-using-loops3621/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void printTillN(int n) {
+        // code here
+        if(n==1)
+        {
+            System.out.print(1 +" ");
+            return;
+        }
+        
+        printTillN(n-1);
+        System.out.print(n+" ");
+    }
+}
