0f4a67940f83ae8acac0b2347f7c7e2ee35463b0
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-23 18:38:14 +0530

    ✅ Count Digits in a Number | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-23

---
+++ gfg/easy/count-total-digits-in-a-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static int countDigits(int n) {
+        // Code here
+        
+        if(n==0)
+        {
+            return 0;
+        }
+        
+        return  1+countDigits(n/10);
+        
+
+    }
+}
