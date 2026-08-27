0f8f6e9b602877f6d3c940e1856ada45c4c8a033
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-27 17:25:05 +0530

    ✅ Employees With Missing Information | Easy | SQL | Runtime: 728 ms (22.89%) | Memory: 0B | 2026-08-27

--- leetcode/easy/1965-employees-with-missing-information/solution.sql
+++ leetcode/easy/1965-employees-with-missing-information/solution.sql
@@ -1,6 +1,17 @@
 # Write your MySQL query statement below
-SELECT e.employee_id FROM Employees e Left JOIN Salaries s ON e.employee_id=s.employee_id where e.employee_id IS NULL 
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Salaries s
+    ON e.employee_id = s.employee_id
+WHERE s.employee_id IS NULL
 
-UNION 
+UNION
 
-SELECT e.employee_id FROM Salaries e Left JOIN Employees s ON e.employee_id=s.employee_id where e.employee_id IS NULL ;
+SELECT e.employee_id
+FROM Salaries e
+LEFT JOIN Employees s
+    ON e.employee_id = s.employee_id
+WHERE s.employee_id IS NULL
+
+ORDER BY employee_id;
+
