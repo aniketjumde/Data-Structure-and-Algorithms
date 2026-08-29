0f7a744e34d85ec8460d11842afe34534213423a
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-08-29 17:14:31 +0530

    ✅ Find Products with Valid Serial Numbers | Easy | SQL | Runtime: 112 ms | Memory: 0B | 2026-08-29

--- leetcode/easy/3465-find-products-with-valid-serial-numbers/solution.sql
+++ leetcode/easy/3465-find-products-with-valid-serial-numbers/solution.sql
@@ -1,2 +1,8 @@
-# Write your MySQL query statement below
-SELECT product_id,product_name,description FROM products WHERE description REGEXP '(^|[^A-Za-z])SN[0-9]{4}-[0-9]{4}([^0-9]|$)' ORDER BY product_id;
+SELECT product_id, product_name, description
+FROM products
+WHERE REGEXP_LIKE(
+    description,
+    '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^A-Za-z0-9]|$)',
+    'c'
+)
+ORDER BY product_id;
