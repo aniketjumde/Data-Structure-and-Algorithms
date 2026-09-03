f7d4c9ba72493272396951972f72dfdb41fd72a6
Aniket Jumde <158746574+aniketjumde@users.noreply.github.com>
2026-09-03 19:25:41 +0530

    ✅ First Unique Even Element | Easy | Java | Runtime: 2 ms (61.94%) | Memory: 46.4 MB | 2026-09-03

--- leetcode/easy/3866-first-unique-even-element/solution.java
+++ leetcode/easy/3866-first-unique-even-element/solution.java
@@ -1,15 +1,16 @@
 class Solution {
     public int firstUniqueEven(int[] nums) 
     {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums)
-        {
-            if(num%2==0)
-            {
-                map.put(num,map.getOrDefault(num,0)+1);
-            }
-        }   
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Step 1: Count frequency of even numbers
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        // Step 2: Find the first unique even number
         for (int num : nums) {
             if (num % 2 == 0 && map.get(num) == 1) {
                 return num;
